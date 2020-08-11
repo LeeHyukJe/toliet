9f04c5e9f0b9f8427eb610fca0d0df3e737a79bb
leehyukje <lims2733@naver.com>
2020-08-12 00:36:29 +0900

    API 호출하여 역 리스트 출력하는 컨트롤러 추가

--- src/main/java/com/wisenut/web/apis/ToiletStationController.java
+++ src/main/java/com/wisenut/web/apis/ToiletStationController.java
@@ -1,10 +1,12 @@
 package com.wisenut.web.apis;
 
+import com.wisenut.domain.IMapInfo;
+import com.wisenut.domain.IMapOffer;
 import com.wisenut.domain.ToiletStationService;
+import com.wisenut.domain.impl.KakaoMapUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,5 +33,17 @@ public class ToiletStationController {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
 
+    }
+
+    @GetMapping("api/search")
+    public ResponseEntity<List<? extends IMapInfo>> searchToiletStation(String stationName){
+        try{
+            List<? extends IMapInfo> iMapInfos = toiletStationService.search(stationName);
+            ResponseEntity<List<? extends IMapInfo>> entity = new ResponseEntity(iMapInfos,HttpStatus.OK);
+            return entity;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 }
