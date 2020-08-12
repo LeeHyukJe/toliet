7c03875a985c5f60d9d55dfac266651feb22e808
leehyukje <lims2733@naver.com>
2020-08-13 00:51:41 +0900

    add method calculate for distance

--- src/main/java/com/wisenut/web/apis/ToiletStationController.java
+++ src/main/java/com/wisenut/web/apis/ToiletStationController.java
@@ -1,15 +1,15 @@
 package com.wisenut.web.apis;
 
-import com.wisenut.domain.IMapInfo;
-import com.wisenut.domain.IMapOffer;
-import com.wisenut.domain.ToiletStationService;
-import com.wisenut.domain.impl.KakaoMapUser;
+import com.wisenut.domain.model.IMapInfo;
+import com.wisenut.domain.application.ToiletStationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ToiletStationController {
@@ -20,27 +20,23 @@ public class ToiletStationController {
         this.toiletStationService = toiletStationService;
     }
 
-    @GetMapping("/api/toiletstations")
-    public ResponseEntity<List<String>> selectToiletStation(){
-        ResponseEntity<List<String>> entity;
-        try{
-            List<String> result = toiletStationService.readToiletStation();
-            entity = new ResponseEntity<>(result, HttpStatus.OK);
-
-            return entity;
-        }catch (Exception e){
-            e.printStackTrace();
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
-
-    }
-
     @GetMapping("api/search")
     public ResponseEntity<List<? extends IMapInfo>> searchToiletStation(String stationName){
         try{
             List<? extends IMapInfo> iMapInfos = toiletStationService.search(stationName);
             ResponseEntity<List<? extends IMapInfo>> entity = new ResponseEntity(iMapInfos,HttpStatus.OK);
             return entity;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("api/calculation/distance")
+    public ResponseEntity<List<? extends IMapInfo>> calculateDistance(@RequestBody Map<String,Object> params){
+        try{
+            IMapInfo iMapInfo = toiletStationService.calculateDistance(params);
+            return new ResponseEntity(iMapInfo,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
