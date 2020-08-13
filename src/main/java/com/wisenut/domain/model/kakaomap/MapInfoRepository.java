0bd4e8e3e4335697a0d02d2563c5a08e263d08fe
leehyukje <lims2733@naver.com>
2020-08-13 16:59:55 +0900

    @Repository annotation added

--- src/main/java/com/wisenut/domain/model/kakaomap/MapInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wisenut.domain.model.kakaomap;
-
-import com.wisenut.domain.model.kakaomap.KaKaoMapInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MapInfoRepository extends JpaRepository<KaKaoMapInfo,Long> {
-}
