be50e813b15d728f83cac8adda7445734528f3c3
leehyukje <lims2733@naver.com>
2020-08-14 15:59:45 +0900

    커맨드 패턴을 사용함에 따라 매개변수 타입 변경

--- src/main/java/com/wisenut/web/apis/ToiletStationController.java
+++ src/main/java/com/wisenut/web/apis/ToiletStationController.java
@@ -2,9 +2,11 @@ package com.wisenut.web.apis;
 
 import com.wisenut.domain.model.IMapInfo;
 import com.wisenut.domain.application.ToiletStationService;
+import com.wisenut.web.payload.SearchPayload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +22,10 @@ public class ToiletStationController {
         this.toiletStationService = toiletStationService;
     }
 
-    @GetMapping("api/search")
-    public ResponseEntity<List<? extends IMapInfo>> searchToiletStation(String stationName){
+    @GetMapping("/api/search")
+    public ResponseEntity<List<? extends IMapInfo>> searchToiletStation(@ModelAttribute SearchPayload payload){
         try{
-            List<? extends IMapInfo> iMapInfos = toiletStationService.search(stationName);
+            List<? extends IMapInfo> iMapInfos = toiletStationService.search(payload.toCommand());
             ResponseEntity<List<? extends IMapInfo>> entity = new ResponseEntity(iMapInfos,HttpStatus.OK);
             return entity;
         }catch (Exception e){
@@ -32,10 +34,11 @@ public class ToiletStationController {
         }
     }
 
-    @GetMapping("api/calculation/distance")
-    public ResponseEntity<List<? extends IMapInfo>> calculateDistance(@RequestBody Map<String,Object> params){
+    @GetMapping("/api/calculation/distance")
+    public ResponseEntity<List<? extends IMapInfo>> calculateDistance(@RequestBody Map<String, Object> params){
         try{
-            IMapInfo iMapInfo = toiletStationService.calculateDistance(params);
+
+            IMapInfo iMapInfo = toiletStationService.calculateDistance(null);
             return new ResponseEntity(iMapInfo,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
