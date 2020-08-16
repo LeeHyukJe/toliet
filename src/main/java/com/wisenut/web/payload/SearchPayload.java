01f7f71923310f7a0b54760f0ac12c9695a5701f
leehyukje <lims2733@naver.com>
2020-08-16 10:14:10 +0900

    payload 필드 추가

--- src/main/java/com/wisenut/web/payload/SearchPayload.java
+++ src/main/java/com/wisenut/web/payload/SearchPayload.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -17,7 +18,23 @@ public class SearchPayload {
     @NotNull
     private String stationName;
 
+    @Size(min = 2, message = "주소 이름은 null 값일 수 없습니다.")
+    @NotNull
+    private String address_name;
+
+    @Size(min = 2, message = "위치 이름은 null 값일 수 없습니다.")
+    @NotNull
+    private String place_name;
+
+    @Size(min=2, message = "좌표값은 빈 값일 수 없습니다.")
+    @NotNull
+    private String x;
+
+    @Size(min = 2, message = "죄표값은 빈 값일 수 없습니다.")
+    @NotNull
+    private String y;
+
     public SearchCommand toCommand(){
-        return new SearchCommand(this.stationName);
+        return new SearchCommand(this.stationName,this.address_name,this.place_name,this.x, this.y);
     }
 }
