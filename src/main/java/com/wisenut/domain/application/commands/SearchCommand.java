86fc48ae9c01e7d0442d2797490a643b0d2251bf
leehyukje <lims2733@naver.com>
2020-08-16 10:13:56 +0900

    command 패턴에 맵핑될 필드 추가

--- src/main/java/com/wisenut/domain/application/commands/SearchCommand.java
+++ src/main/java/com/wisenut/domain/application/commands/SearchCommand.java
@@ -10,9 +10,16 @@ import lombok.ToString;
 public class SearchCommand {
 
     private String stationName;
+    private String addressName;
+    private String placeName;
+    private String x;
+    private String y;
 
-    public SearchCommand(String stationName){
+    public SearchCommand(String stationName,String addressName, String placeName, String x, String y){
         this.stationName = stationName;
+        this.addressName = addressName;
+        this.x = x;
+        this.y = y;
     }
 
 
