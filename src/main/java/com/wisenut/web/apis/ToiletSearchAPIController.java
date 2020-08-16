9b187ef03918f1cea3066178c720d53c605c2378
leehyukje <lims2733@naver.com>
2020-08-16 10:15:24 +0900

    가장 가까운 역을 찾기 위한 매개변수 변경

---
+++ src/main/java/com/wisenut/web/apis/ToiletSearchAPIController.java
@@ -0,0 +1,46 @@
+package com.wisenut.web.apis;
+
+import com.wisenut.domain.model.IMapInfo;
+import com.wisenut.domain.application.ToiletStationService;
+import com.wisenut.domain.model.kakaomap.KaKaoMapInfo;
+import com.wisenut.web.payload.SearchPayload;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ToiletSearchAPIController {
+
+    private ToiletStationService toiletStationService;
+
+    public ToiletSearchAPIController(ToiletStationService toiletStationService){
+        this.toiletStationService = toiletStationService;
+    }
+
+    @GetMapping("/api/search")
+    public ResponseEntity<List<? extends IMapInfo>> searchToiletStation(@ModelAttribute SearchPayload payload){
+        try{
+            List<? extends IMapInfo> iMapInfos = toiletStationService.search(payload.toCommand());
+            ResponseEntity<List<? extends IMapInfo>> entity = new ResponseEntity(iMapInfos,HttpStatus.OK);
+            return entity;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/api/calculation/distance")
+    public ResponseEntity<String> calculateDistance(@RequestBody SearchPayload payload){
+        try{
+
+            String iMapInfo = toiletStationService.searchNearestStationName(payload.toCommand());
+            return new ResponseEntity(iMapInfo,HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
