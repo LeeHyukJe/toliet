d540a41654e6114cb639ec86ce44aa227b40d88a
lims2733@naver.com <ks157680>
2020-08-20 17:12:19 +0900

    역 리스트 가져오는 스케줄러 구현

---
+++ src/main/java/com/wisenut/config/ScheduleCollectStationName.java
@@ -0,0 +1,21 @@
+package com.wisenut.config;
+
+import com.wisenut.domain.application.ToiletStationService;
+import com.wisenut.domain.model.IMapOffer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleCollectStationName {
+
+    private ToiletStationService toiletStationService;
+    public ScheduleCollectStationName(ToiletStationService toiletStationService){
+        this.toiletStationService = toiletStationService;
+    }
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void collectToiletStationName(){
+        toiletStationService.createToiletStation();
+    }
+}
