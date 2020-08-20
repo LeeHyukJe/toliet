f72d73a1b1ffd27cfc90ccbf9e2063e03e125a8e
lims2733@naver.com <ks157680>
2020-08-20 18:53:33 +0900

    배치 설정

--- src/main/java/com/wisenut/ToiletApplication.java
+++ src/main/java/com/wisenut/ToiletApplication.java
@@ -1,11 +1,13 @@
 package com.wisenut;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableBatchProcessing
 public class ToiletApplication {
 
     public static void main(String[] args) {
