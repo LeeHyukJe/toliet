7aaba49daffcf49c14fa2a747f6b65d00e75decb
lims2733@naver.com <ks157680>
2020-08-20 17:12:45 +0900

    JPA 더티체킹 테스트

--- src/test/java/com/wisenut/toliet/ToiletJPATest.java
+++ src/test/java/com/wisenut/toliet/ToiletJPATest.java
@@ -10,8 +10,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ToiletJPATest {
 
     @Autowired
@@ -20,16 +24,33 @@ public class ToiletJPATest {
 
     @Test
     public void db_insert_test_should_success(){
-//        KaKaoMapInfo kaKaoMapInfo = KaKaoMapInfo.builder().id(1l).category_group_code("SW8").category_group_name("지하철역")
-//                .category_name("교통,수송 > 지하철,전철 > 수도권4호선").phone("02-6110-4251").place_name("회현역 4호선")
-//                .place_url("http://place.map.kakao.com/21160642").road_address_name("서울 중구 퇴계로 지하 54")
-//                .address_name("서울 중구 남창동 116-1")
-//                .x("126.97843725718793")
-//                .y("37.558760244882336").build();
-//
-//        kaKaoMapInfoRepository.save(kaKaoMapInfo);
-//
-//        KaKaoMapInfo info = kaKaoMapInfoRepository.findById(1l).get();
-//        Assert.assertEquals(info.getAddress_name(),"서울 중구 남창동 116-1");
+        KaKaoMapInfo kaKaoMapInfo = KaKaoMapInfo.builder().id(21160642l).categoryGroupCode("SW8").categoryGroupName("지하철역")
+                .categoryName("교통,수송 > 지하철,전철 > 수도권4호선").phone("02-6110-4251").placeName("회현역 4호선")
+                .placeUrl("http://place.map.kakao.com/21160642").roadAddressName("서울 중구 퇴계로 지하 54")
+                .addressName("서울 중구 남창동 116-1")
+                .x("126.97843725718793")
+                .y("37.558760244882336").build();
+
+
+        kaKaoMapInfoRepository.save(kaKaoMapInfo);
+
+        KaKaoMapInfo info = kaKaoMapInfoRepository.findById(21160642l).get();
+        Assert.assertEquals(info.getAddressName(),"서울 중구 남창동 116-1");
+    }
+
+    @Test
+    public void test3(){
+        List<KaKaoMapInfo> list = kaKaoMapInfoRepository.findAll();
+
+        KaKaoMapInfo kaKaoMapInfo = KaKaoMapInfo.builder().id(21160642l).categoryGroupCode("SW8").categoryGroupName("지하철역")
+                .categoryName("교통,수송 > 지하철,전철 > 수도권4호선").phone("02-6110-4251").placeName("회현역 4호선")
+                .placeUrl("http://place.map.kakao.com/21160642").roadAddressName("서울 중구 퇴계로 지하 54")
+                .addressName("서울 중구 남창동 116-1")
+                .x("126.97843725718793")
+                .y("37.558760244882336").build();
+
+        list = list.stream().filter(ele -> !kaKaoMapInfoRepository.existsById(ele.getId())).collect(Collectors.toList());
+        Assert.assertEquals(list.size(), 0);
+
     }
 }
