e3f9236336f6f0368796b1b62ecb2ceb4e6f4a3c
leehyukje <lims2733@naver.com>
2020-08-28 17:35:10 +0900

    User Entity 추가

--- src/main/java/com/wisenut/domain/model/kakaomap/KakaoMapUser.java
+++ src/main/java/com/wisenut/domain/model/kakaomap/KakaoMapUser.java
@@ -2,10 +2,40 @@ package com.wisenut.domain.model.kakaomap;
 
 import com.wisenut.domain.model.IMapOffer;
 import com.wisenut.domain.model.IUser;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.*;
+import java.util.Date;
+
 @Component
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class KakaoMapUser implements IUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="username", nullable = false, length = 50, unique = true)
+    private String username;
+    @Column(name="email_address", nullable = false, length = 100, unique = true)
+    private String emailAddress;
+    @Column(name="password", nullable = false, length = 30)
+    private String password;
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date", nullable = false)
+    private Date createdDate;
+
+
+    @Transient
     private IMapOffer iMapOffer;
 
     public KakaoMapUser(IMapOffer iMapOffer){
