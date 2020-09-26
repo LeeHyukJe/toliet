f0594446019c9d5220112d687a5cdf81ab8aaa95
leehyukje <lims2733@naver.com>
2020-09-26 12:39:07 +0900

    add back-end request mapping

--- src/main/java/com/wisenut/web/pages/MainController.java
+++ src/main/java/com/wisenut/web/pages/MainController.java
@@ -1,10 +1,12 @@
 package com.wisenut.web.pages;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 
-    @GetMapping(value = {"/"})
+    @GetMapping(value = {"/", "/login"})
     public String entry(){
         return "index";
     }
